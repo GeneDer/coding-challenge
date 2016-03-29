014bfad1d4c28116fc38fd3a3f1a012113c70860
aouyang1 <aouyang1@gmail.com>
2016-03-29 01:12:56 -0700

    updated run.sh to average_degree.py and updated tweet_output to reflect only output.txt

--- run.sh
+++ run.sh
@@ -3,8 +3,7 @@
 # example of the run script for running the word count
 
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
-python ./src/words_tweeted.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt
-python ./src/median_unique.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt
+python ./src/average_degree.py ./tweet_input/tweets.txt ./tweet_output/output.txt
 
 
 
